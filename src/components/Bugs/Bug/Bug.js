0628e78eda48c46c9818b48767648a8787ab73a4
davebanguilan <desbanguilan@gmail.com>
2021-02-08 00:32:58 +0800

    Modified styles in Bug/styles.js

--- src/components/Bugs/Bug/Bug.js
+++ src/components/Bugs/Bug/Bug.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from '@material-ui/icons/Edit';
-
+import ScheduleOutlinedIcon from '@material-ui/icons/ScheduleOutlined';
 import moment from 'moment';
 import useStyles from './styles.js';
 
@@ -10,23 +10,39 @@ const Bug = ({bug}) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            <div className={classes.media}></div>
+            <div className={classes.media}>
+
+            </div>
             <div className={classes.overlay}>
-                <Typography variant="h5">{bug.title}</Typography>
-                <Typography variant="body2">{moment(bug.createdAt).fromNow()}</Typography>
+                <Typography variant="h4">{bug.title}</Typography>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                    marginTop: '10px'
+                }}>
+                
+                    <ScheduleOutlinedIcon/>
+                    &nbsp;
+                    <Typography variant="h6">{moment(bug.createdAt).fromNow()}</Typography>
+
+                </div>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color: 'black'}} size="small" onClick={()=>{}}>
+                <Button style={{color: 'blue'}} size="small" onClick={()=>{}}>
                     <EditIcon fontSize="default" />
                 </Button>
             </div>
-            <div className={classes.details}>
-                <Typography variant="subtitle2" color="textSecondary">Members: </Typography>
-                <Typography variant="subtitle2" color="textSecondary">{bug.members.map((member, i) => bug.members.length === i + 1 ? `${member}` : `${member}, `)}</Typography>
-            </div>
             <CardContent>
-                <Typography className={classes.creator}>{bug.creator}</Typography>
+                <Typography variant="h5" className={classes.creator}>{bug.creator}</Typography>
+                <div className={classes.details}>
+                    <Typography variant="h6" color="textSecondary">Members: </Typography>
+                    <Typography variant="h6" color="textSecondary">{bug.members.map((member, i) => bug.members.length === i + 1 ? `${member}` : `${member}, `)}</Typography>
+                    <Typography variant="h6" color="textSecondary" style={{color: bug.severity === "High" ? "red" : "textSecondary" }}>Severity: {bug.severity}</Typography>
+                    <Typography variant="h6" color="textSecondary" style={{color: bug.status === "Completed" ? "green" : "textSecondary" }}>Status: {bug.status}</Typography>
+                </div>
             </CardContent>
+            
             <CardActions>
                 <Button size="small" color="secondary" onClick={()=>{}}>
                     <DeleteIcon />
--- src/components/Bugs/Bug/styles.js
+++ src/components/Bugs/Bug/styles.js
@@ -1,4 +1,36 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
+    media: {
+        height: 0,
+        paddingTop: "30%",
+        backgroundColor: "rgba(153, 153, 153, 0.5)",
+        backgroundBlendMode: "darken",
+    },
+    border: {
+        border: "solid",
+    },
+    fullHeightCard: {
+        height: "100%",
+    },
+    card: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        borderRadius: "15px",
+        height: "100%",
+        position: "relative",
+    },
+    overlay: {
+        position: "absolute",
+        top: "20px",
+        left: "20px",
+        color: "black",
+    },
+    overlay2: {
+        position: "absolute",
+        top: "25px",
+        right: "15px",
+        color: "white",
+    },
 }));
